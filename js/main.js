0602321b7622ca08e57d7cf324f063c12a98fe13
Nepal Gain <nepalgain11@gmail.com>
2021-02-07 23:52:14 +0600

    update

--- js/main.js
+++ js/main.js
@@ -2,47 +2,38 @@ document.getElementById("button").addEventListener("click",function(){
     const inputValue = document.getElementById("input-area").value;
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
     .then(res => res.json())
-    .then (data =>displayFood(data.meals))
+    .then (data =>displayFood(data.meals)) // object to array convert
 });
 
 const displayFood = meals =>{
-    
     const items = document.querySelector(".items");
-    // const refresh = document.querySelector("#input-area");
-    // refresh.innerHTML = "";
     meals.map (item => {
-       
         const itemDiv = document.createElement("div");
         itemDiv.className = "item-class"; 
-        //const foodImg = meal.strMealThumb;
+        //thumbnail
         const foodInfo = `
-            <img class="image-size"  src = "${item.strMealThumb}">
-            <h5>${item.strMeal}</h5>   
+            <img id="more_details" class="image-size" data-bs-toggle="modal" data-bs-target="#exampleModal" src = "${item.strMealThumb}"> 
+            <h5>${item.strMeal}</h5>
         `
-        //<button onclick="itemDetails('${item.idMeal}')">Show More</button>
+        const modelDiv = document.querySelector(".modal-body");
+                    //food details 
+        const foodDetails = ` 
+            <li>${item.strIngredient1}</li>
+            <li>${item.strIngredient2}</li>
+            <li>${item.strIngredient3}</li>
+             <li>${item.strIngredient4}</li>
+             <li>${item.strIngredient5}</li>
+             <li>${item.strIngredient7}</li>
+        
+        `
+        modelDiv.innerHTML = foodDetails;
         itemDiv.innerHTML = foodInfo;
        items.appendChild(itemDiv);
     });
-    
+
 }
 
-    const itemDetails = details => {
-        //const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
-        //const foodImg = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
-        console.log(details);
-    }
-
-
-
-
-
-
-
-// <img src ="${foodImg}" >
-
-
-
-
+ 
 
 
 
